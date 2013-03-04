5308372842ee9028570b5fe30e4297fe3d7ae15f
Kingme <gycm520@gmail.com>
2013-03-04 18:49:07 +0800

    rewrite some scripts for my own ubuntu 12.10

--- android_compile.sh
+++ android_compile.sh
@@ -1,10 +1,12 @@
+TCHAIN=~/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-
+
 ./android_clean.sh
 
-echo "168" > .version
-make ARCH=arm CROSS_COMPILE=~/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi- ap33_android_defconfig
-make ARCH=arm CROSS_COMPILE=~/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi- -j12
-make ARCH=arm CROSS_COMPILE=~/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi- -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` clean -j20
-make ARCH=arm CROSS_COMPILE=~/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi- -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` -j20
+make ARCH=arm CROSS_COMPILE=$TCHAIN ap33_android_defconfig
+date '+%Y%m%d%H' > .version
 
+make ARCH=arm CROSS_COMPILE=$TCHAIN -j12
+make ARCH=arm CROSS_COMPILE=$TCHAIN -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` -j20
+sh ./copy_modules.sh
 
 
--- copy_modules.sh
+++ copy_modules.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-
-cp ./arch/arm/boot/zImage ~/android/modules/zImage
-find . -name "*.ko" -exec cp {} ~/android/modules \;
+rm ~/patchrom/Others/kit/BOOT-EXTRACTED2/zImage
+rm -r ~/patchrom/Others/kit/BOOT-EXTRACTED
+rm -r ~/patchrom/Others/kit/exm_kernel/system/lib/modules/
+rm ~/patchrom/Others/kit/exm_kernel/boot.img
+rm ~/patchrom/Others/kit/exm_kernel/WORKING_030213_005518/boot.img 
+mkdir ~/patchrom/Others/kit/exm_kernel/system/lib/modules
+cp ./arch/arm/boot/zImage ~/patchrom/Others/kit/BOOT-EXTRACTED2/zImage
+cp -r ~/patchrom/Others/kit/BOOT-EXTRACTED2 ~/patchrom/Others/kit/BOOT-EXTRACTED
+find . -name "*.ko" -exec cp {} ~/patchrom/Others/kit/exm_kernel/system/lib/modules \;
+cd ~/patchrom/Others/kit
+./menu
